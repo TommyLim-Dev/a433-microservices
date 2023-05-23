36074182d04b0d0e0521f77002d4e656e1cc3cac
unknown <tommy199915@gmail.com>
2023-05-23 20:09:23 +0700

    Add build.sh file

---
+++ build_push_image_karsajobs.sh
@@ -0,0 +1,16 @@
+#Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image tommy_7scr/karsajobs, dan memiliki tag latest.
+docker build -t tommy_7scr/karsajobs:latest .
+ 
+#Melihat daftar image di lokal.
+docker images
+ 
+#Mengubah nama image agar sesuai dengan format GitHub Packages 
+docker tag tommy_7scr/karsajobs:latest ghcr.io/tommylim-dev/tommy_7scr/karsajobs:latest
+ 
+#Login ke github packages
+echo $PASSWORD_DOCKER_HUB | docker login ghcr.io --username TommyLim-Dev --password-stdin
+ 
+#Mengunggah image GitHub Packages 
+docker push ghcr.io/tommylim-dev/tommy_7scr/karsajobs:latest
+
+docker run -d -p 8080:8080 ghcr.io/tommylim-dev/tommy_7scr/karsajobs
